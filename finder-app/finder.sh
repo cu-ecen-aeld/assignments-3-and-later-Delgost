7aefee89379887b6d1171887c2c5998d82d550d3
Delgost <35934787+Delgost@users.noreply.github.com>
2024-10-22 21:53:14 +0200

    bugfix for finder.sh
    Substract current directory reference in files count

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -12,17 +12,15 @@ readonly filesdir=$1
 readonly searchstr=$2
 
 # Check number of arguemnts
-if [ "$#" -ne 2 ]
-    then
-        echo "Not correct number of arguments supplied. Expected number of arguments: 2"
-        echo "Usage: $0 <path-to-directory> <search-string>"
-        exit 1
+if [ "$#" -ne 2 ]; then
+    echo "Not correct number of arguments supplied. Expected number of arguments: 2"
+    echo "Usage: $0 <path-to-directory> <search-string>"
+    exit 1
 fi
 
 # Verify directory argument and perform action
-if [ -d $1 ]
-then 
-    number_of_files=$(find $filesdir | wc -l)
+if [ -d $1 ]; then 
+    number_of_files=$(( $(find $filesdir | wc -l) - 1 )) # -1 : Current directory reference doesn't count
     number_of_matching_lines=$(grep -R $searchstr $filesdir | wc -l) 
     echo "The number of files are $number_of_files and the number of matching lines are $number_of_matching_lines."
 else
