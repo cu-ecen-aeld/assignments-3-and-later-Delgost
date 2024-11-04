d01c121bb36bb1eb06e8dfb401de263e08bcabc9
Delgost <35934787+Delgost@users.noreply.github.com>
2024-11-04 19:19:53 +0100

    resolve TODOs in manual-linux.sh according to assignment 3 part 2 instructions

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 #
 # Script find.sh for Assignment 1.
 # Features:
--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -12,6 +12,7 @@ BUSYBOX_VERSION=1_33_1
 FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
 CROSS_COMPILE=aarch64-none-linux-gnu-
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 if [ $# -lt 1 ]
 then
@@ -33,11 +34,31 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     cd linux-stable
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
+    
+    #Clean kernel build tree
+    echo "Clean kernel build tree"
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
 
-    # TODO: Add your kernel build steps here
+    #Deconfig
+    echo "Deconfig..."
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+
+    #Build vmlinux
+    echo "Build vmlinux..."
+    make -j10 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
+
+    #Build modules
+    #echo "Build modules..."
+    #make -j10 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules
+
+    #Build devicetree
+    echo "Build devicetree..."
+    make -j10 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
+
 fi
 
 echo "Adding the Image in outdir"
+cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}/
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
@@ -47,7 +68,13 @@ then
     sudo rm  -rf ${OUTDIR}/rootfs
 fi
 
-# TODO: Create necessary base directories
+echo "Create roofts folder tree..."
+mkdir -p ${OUTDIR}/rootfs
+cd "$OUTDIR/rootfs"
+
+mkdir -p bin dev etc root home lib lib64 proc sbin sys tmp usr var
+mkdir -p usr/bin usr/lib usr/sbin
+mkdir -p var/log
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -55,26 +82,53 @@ then
 git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
-    # TODO:  Configure busybox
+
+    echo "Configure Busybox..."
+    make distclean
+    make defconfig
+   
 else
     cd busybox
+
+    echo "Configure Busybox..."
+    make distclean
+    make defconfig
+
 fi
 
-# TODO: Make and install busybox
+echo "Make and install busybox..."
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
+
+cd "$OUTDIR/rootfs"
 
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
-# TODO: Add library dependencies to rootfs
+echo "Add library dependencies to rootfs..."
+cp ${HOME}/aarch64_crosscompile_toolchain/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib/
+cp ${HOME}/aarch64_crosscompile_toolchain/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libm.so.6 ${OUTDIR}/rootfs/lib64/
+cp ${HOME}/aarch64_crosscompile_toolchain/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libresolv.so.2 ${OUTDIR}/rootfs/lib64/
+cp ${HOME}/aarch64_crosscompile_toolchain/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libc.so.6 ${OUTDIR}/rootfs/lib64/
 
-# TODO: Make device nodes
+echo "Clean and build the writer utility"
+cd "$SCRIPT_DIR"
+make clean
+make CROSS_COMPILE=${CROSS_COMPILE}
 
-# TODO: Clean and build the writer utility
+ech "Copy the finder related scripts and executables to the /home directory on the target rootfs..."
+cp $(dirname "$0")/writer ${OUTDIR}/rootfs/home/
+cp $(dirname "$0")/finder.sh ${OUTDIR}/rootfs/home/
+cp -r $(dirname "$0")/conf/ ${OUTDIR}/rootfs/home/
+cp $(dirname "$0")/finder-test.sh ${OUTDIR}/rootfs/home/
+cp $(dirname "$0")/autorun-qemu.sh ${OUTDIR}/rootfs/home/
 
-# TODO: Copy the finder related scripts and executables to the /home directory
-# on the target rootfs
+echo "chown the root directory..."
+sudo chown -R root ${OUTDIR}/rootfs/root
 
-# TODO: Chown the root directory
-
-# TODO: Create initramfs.cpio.gz
+echo "Create initrmfs.cpio.gz..."
+cd "$OUTDIR/rootfs"
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+cd "$OUTDIR"
+gzip -f initramfs.cpio
